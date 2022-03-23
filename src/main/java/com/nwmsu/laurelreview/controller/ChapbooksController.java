e719009768872331025f42fa620762b47f152632
Nagateja Neerukonda <77507758+TejaUsa@users.noreply.github.com>
2022-03-22 21:18:05 -0500

    chapbooks Api updated

--- src/main/java/com/nwmsu/laurelreview/controller/ChapbooksController.java
+++ src/main/java/com/nwmsu/laurelreview/controller/ChapbooksController.java
@@ -1,5 +1,70 @@
+
 package com.nwmsu.laurelreview.controller;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nwmsu.laurelreview.model.ChapBook;
+import com.nwmsu.laurelreview.repo.ChapBookRepo;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+
+@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
+@RestController
+@RequestMapping("api")
 public class ChapbooksController {
+	private final ChapBookRepo chapBookRepo;
+
+	@Autowired
+	public ChapbooksController(final ChapBookRepo chapBookRepo) {
+		this.chapBookRepo = chapBookRepo;
+	}
+	
+	@GetMapping(value = "/chapbooks")
+	public List<ChapBook> getposCasts(){
+		return chapBookRepo.findAll();
+	}
+	
+	@PostMapping(value = "/chapbook/save")
+	public String savePoscast(@RequestBody ChapBook chapbook) {
+		chapBookRepo.save(chapbook);
+		return "chapbook saved successfully";
+	}
+	
+	@GetMapping(value = "/chapbook/{id}")
+	public ChapBook getchapbookById(@PathVariable Long id) {
+		return chapBookRepo.findById(id).get();
+	}
+	
+	@PutMapping(value = "chapbook/update/{id}")
+	public String updatechapbook(@PathVariable Long id,@RequestBody ChapBook updatedchapbook) {
+		ChapBook chapbook = chapBookRepo.findById(id).orElse(null);
+		if (chapbook == null) {
+			return "chapbook with "+ id + " does not exist";
+		}else {
+			updatedchapbook.setchapbookId(id);
+			BeanUtils.copyProperties(updatedchapbook, chapbook);
+			this.savePoscast(chapbook);
+			return "chapbook updated";
+		}
+	}
+	
+	@DeleteMapping(value = "chapbook/delete/{id}")
+	public String deletechapbook(@PathVariable Long id) {
+		chapBookRepo.deleteById(id);
+		return "Deleted Successfully";
+	}
+	
 
 }
--- src/main/java/com/nwmsu/laurelreview/model/ChapBook.java
+++ src/main/java/com/nwmsu/laurelreview/model/ChapBook.java
@@ -1,5 +1,66 @@
+
+
+
 package com.nwmsu.laurelreview.model;
 
-public class ChapBook {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ChapBook implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long chapBookId;
+	@Column
+	private String chapBookName;
+	@Column
+	private String chapBookDesc;
+	@Column
+	private String chapBookDate;
+	@Column
+	private String chapBookAudioFile;
+	@Column
+	private String chapBookCoverImage;
+	public Long getchapBookId() {
+		return chapBookId;
+	}
+	public void setchapBookId(Long chapBookId) {
+		this.chapBookId = chapBookId;
+	}
+	public String getchapBookName() {
+		return chapBookName;
+	}
+	public void setchapBookName(String chapBookName) {
+		this.chapBookName = chapBookName;
+	}
+	public String getchapBookDesc() {
+		return chapBookDesc;
+	}
+	public void setchapBookDesc(String chapBookDesc) {
+		this.chapBookDesc = chapBookDesc;
+	}
+	public String getchapBookDate() {
+		return chapBookDate;
+	}
+	public void setchapBookDate(String chapBookDate) {
+		this.chapBookDate = chapBookDate;
+	}
+	public String getchapBookAudioFile() {
+		return chapBookAudioFile;
+	}
+	public void setchapBookAudioFile(String chapBookAudioFile) {
+		this.chapBookAudioFile = chapBookAudioFile;
+	}
+	public String getchapBookCoverImage() {
+		return chapBookCoverImage;
+	}
+	public void setchapBookCoverImage(String chapBookCoverImage) {
+		this.chapBookCoverImage = chapBookCoverImage;
+	}
 
 }
